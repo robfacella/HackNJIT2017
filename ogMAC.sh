e58e6958f2d2e870f465e640eeae33cb9935f1a5
root <root@localhost.localdomain>
2017-11-11 17:13:13 -0500

    MAC reset and random spoof.

---
+++ ogMAC.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ifconfig wlan0 down
+macchanger -p wlan0
+ifconfig wlan0 up
---
+++ randMAC.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ifconfig wlan0 down
+macchanger -a wlan0
+ifconfig wlan0 up
